7e0ad6881926c8b58d65db6937e6ee451ee1bf0c
llinauer <l.linauer@gmx.at>
2023-06-23 08:39:59 +0200

    Create model.py and add MiniGPT class template

---
+++ gpt_mini/model.py
@@ -0,0 +1,18 @@
+"""
+model.py
+
+Definition of the transformer model
+
+"""
+
+import torch
+import torch.nn as nn
+import einops
+
+
+class MiniGPT(nn.model):
+    """ GPT-style transformer model """
+
+    def __init__(self):
+        pass
+
