693e040d0b57eced84aa0a334bce8a7285ea905f
llinauer <l.linauer@gmx.at>
2023-06-30 08:58:00 +0200

    Rename model_size -> d_model to be consistent, Fix LayerNorm.forward docstring

--- gpt_mini/model.py
+++ gpt_mini/model.py
@@ -39,13 +39,13 @@ class LayerNorm(nn.Module):
         """ Normalize the input over all neurons in the layer. Then apply scaling with gamma and translate
             with beta.
 
-        :param inputs: torch.tensor(batch_size, position, model_size), layer inputs
-        :return: torch.tensor(batch_size, position, model_size), layer normalization of inputs
+        :param inputs: torch.tensor(batch_size, position, d_model), layer inputs
+        :return: torch.tensor(batch_size, position, d_model), layer normalization of inputs
         """
 
-        # calculate the mean and variance over all neurons in the layer ( = model_size dimension)
-        mean = einops.reduce(inputs, 'batch position model_size -> batch position 1', 'mean')
-        var = einops.reduce(torch.pow(inputs - mean, 2), 'batch position model_size -> batch position 1', 'mean')
+        # calculate the mean and variance over all neurons in the layer ( = d_model dimension)
+        mean = einops.reduce(inputs, 'batch position d_model -> batch position 1', 'mean')
+        var = einops.reduce(torch.pow(inputs - mean, 2), 'batch position d_model -> batch position 1', 'mean')
 
         # subtract the mean, divide by sqrt(var) and transform
         normalized_inputs = (inputs - mean) / torch.sqrt(var + self.epsilon)
@@ -72,8 +72,8 @@ class Embedding(nn.Module):
         """ Embed the input tokens. The input tokens are integers; we want to select for each token
             the row of the W_E matrix with that index.
 
-        :param tokens: torch.tensor(batch, position d_vocab), input token tensor
-        :return: torch.tensor(batch, position d_model), embedding tensor
+        :param tokens: torch.tensor(batch, position d_vocab), input tokens
+        :return: torch.tensor(batch, position d_model), embeddings
         """
 
         embeddings = self.W_E[tokens]
@@ -84,15 +84,15 @@ class PositionalEmbedding(nn.Module):
     """ Positional Embedding:
     """
 
-    def __init__(self, context_length, model_size, init_std=0.02):
+    def __init__(self, context_length, d_model, init_std=0.02):
         """ Initialize the positional embedding layer with the W_pos matrix
 
         :param context_length: int, context length of the transformer
-        :param model_size: int, size of the transformer model
+        :param d_model: int, size of the transformer model
         :param init_std: float, standard deviation for initializing the weights
         """
         super().__init__()
-        self.W_pos = nn.Parameter(torch.empty((context_length, model_size)))
+        self.W_pos = nn.Parameter(torch.empty((context_length, d_model)))
         nn.init.normal_(self.W_pos, std=init_std)
 
     def forward(self, tokens):
