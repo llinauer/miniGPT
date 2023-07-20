b6d4fc99ad61c5e4f4470088ff304468c36197d6
Lukas Linauer <l.linauer@gmx.at>
2023-07-20 16:49:49 +0200

    Modify download_data.py Add main function, add -o/--output_path argument to parse_args

--- download_data.py
+++ download_data.py
@@ -5,6 +5,7 @@ Download the datasets used for training MiniGPT
 """
 
 import argparse
+import datasets
 
 def parse_args():
     """ Parse the command-line args
@@ -13,6 +14,30 @@ def parse_args():
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--dataset', type=str, choices=['pile-10k', 'wikipedia'],
-                        required=True)
+    parser.add_argument('--dataset', type=str, help='Name of datset to download',
+                        choices=['pile-10k', 'de-wikipedia'], required=True)
+    parser.add_argument('-o', '--output_path', type=str,
+                        help='Path to store dataset to. (Default = data)', default='data')
     return parser.parse_args()
+
+
+def main():
+    """ Main function
+        Get name of dataset from command-line args and download it
+    :return: None
+    """
+
+    args = parse_args()
+
+    # load dataset
+    if args.dataset == 'pile-10k':
+        ds = datasets.load_dataset('NeelNanda/pile-10k')
+    elif args.dataset == 'de-wikipedia':
+        ds = datasets.load_dataset('wikipedia', '20220301.de')
+
+    # save dataset to path
+    ds.save_to_disk(args.output_path)
+
+
+if __name__ == '__main__':
+    main()
