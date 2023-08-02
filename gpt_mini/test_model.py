fad208e998c9813bc631dce77c8c4c384c6be4fa
Lukas Linauer <l.linauer@gmx.at>
2023-08-02 14:46:08 +0200

    Add trained_gpt2_test function to test_model; test layers with weights from a trained transformer

--- gpt_mini/test_model.py
+++ gpt_mini/test_model.py
@@ -6,8 +6,9 @@ Test if all the model layers output the correct shape
 """
 
 import torch
-from model import LayerNorm, Embedding, Unembedding, PositionalEmbedding, Attention, MLP   
+from model import LayerNorm, Embedding, Unembedding, PositionalEmbedding, Attention, MLP
 from model import TransformerBlock, MiniGPT
+from transformer_lens import EasyTransformer
 
 
 def rand_float_test(layer, input_shape, expected_shape):
@@ -38,11 +39,32 @@ def rand_int_test(layer, input_shape, expected_shape):
         print(f'FAIL! Output shape {tuple(output.shape)} != {expected_shape}')
 
 
+def trained_gpt2_test(layer, gpt2_layer, weight_map, layer_input):
+    """ Load weights from a fully trained gpt2-style transformer layer
+        and compare the outputs of it to the custom layer on some input tokens
+
+    :param layer: nn.Module, Transformer layer to test
+    :param gpt2_layer: nn.Module, Fully Trained GPT2-style layer
+    :param weight_map: dict, map weight names from the trained layer to the custom layer
+    :param layer_input: torch.tensor, input to the layer
+    :return: None
+    """
+ 
+    # load weights from the trained layer
+    trained_weights = gpt2_layer.state_dict()
+    # map weights
+    mapped_weights = {val: trained_weights[key] for key, val in weight_map.items()}
+
+    layer.load_state_dict(mapped_weights, strict=False)
+    output = layer(layer_input)
+    comparison = torch.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f'{comparison.sum()/comparison.numel():.2%} of the values are correct')
+
 def test_model():
 
     print('Test LayerNorm layer')
     rand_float_test(LayerNorm(768), (2, 4, 768), (2, 4, 768))
-    
+ 
     print('Test Embedding layer')
     rand_int_test(Embedding(768, 4000), (2, 4), (2, 4, 768))
     print('Test Positional embedding layer')
