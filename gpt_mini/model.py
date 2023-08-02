e21937acbc4b634c55fdf6b04bf567f2d0c66695
Lukas Linauer <l.linauer@gmx.at>
2023-08-02 16:14:15 +0200

    Replace torch.nn.GELU with huggingface gelu_new

--- gpt_mini/model.py
+++ gpt_mini/model.py
@@ -9,6 +9,7 @@ import math
 import torch
 from torch import nn
 import einops
+from transformers import activations
 
 
 class LayerNorm(nn.Module):
@@ -254,7 +255,7 @@ class MLP(nn.Module):
         self.b_out = nn.Parameter(torch.zeros(d_model))
 
         # activation
-        self.activation = nn.GELU()
+        self.activation = activations.gelu_new
 
     def forward(self, inputs):
         """
