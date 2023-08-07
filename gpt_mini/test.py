58dd4c780f388d0ea0e263ffd843ce80a7b14917
Lukas Linauer <l.linauer@gmx.at>
2023-08-07 16:58:09 +0200

    Load model and model weights in main function of test.py

--- gpt_mini/test.py
+++ gpt_mini/test.py
@@ -7,7 +7,8 @@ Test the transformer model by letting it generate text
 import argparse
 import torch
 import transformers
-
+from pathlib import Path
+from model import MiniGPT
 
 def parse_args():
     """ Parse the command-line args
@@ -25,6 +26,36 @@ def parse_args():
 def main():
     """ Main function """
     args = parse_args()
+    
+    # create tokenizer
+    tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
+
+    # define model parameters
+    n_layers = 6
+    d_vocab = tokenizer.vocab_size
+    context_length = 1024
+    d_model = 256
+    n_heads = 12
+    d_head = 64
+
+    device = torch.device('cuda')
+    
+    # create model
+    model = MiniGPT(n_layers, d_vocab, context_length, d_model, n_heads, d_head).to(device)
+
+    # load weights
+    weights_path = Path(args.weights)
+    if not weights_path.exists() or not weights_path.is_file():
+        print(f'Could not load weights file: {weights_path}! Please make sure it exists and' 
+               'is a correct weights file')
+        return
+
+    try:
+        model.load_state_dict(torch.load(weights_path), strict=True)
+    except Exception as e:
+        print(e)
+        return
+
 
 
 if __name__ == '__main__':
