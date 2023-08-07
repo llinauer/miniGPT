900ce669124e9db1b0ccd30c4eefd318633bd36d
Lukas Linauer <l.linauer@gmx.at>
2023-08-07 16:51:48 +0200

    Add parse_args function to test.py

--- gpt_mini/test.py
+++ gpt_mini/test.py
@@ -9,3 +9,14 @@ import torch
 import transformers
 
 
+def parse_args():
+    """ Parse the command-line args
+
+    :return: argparse.Namespace obj, the parsed arguments
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, help='Path to the weights file', required=True)
+    parser.add_argument('--decoding-method', type=str, choices=['greedy', 'beam', 'sample'],
+                        help='Method to decode the transformer output')
+    return parser.parse_args()
