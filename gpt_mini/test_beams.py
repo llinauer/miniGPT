d6c2802428cfc903c9bd691130d785a0d7a21c9f
Lukas Linauer <l.linauer@gmx.at>
2023-08-09 16:55:39 +0200

    Load trained GPT2 weighs for testing in test_beams.py

--- gpt_mini/test_beams.py
+++ gpt_mini/test_beams.py
@@ -8,6 +8,7 @@ import torch
 import transformers
 from model import MiniGPT
 from generate import Beams
+from transformer_lens import EasyTransformer
 
 
 def test_beam_generation(beams):
@@ -18,11 +19,49 @@ def test_beam_generation(beams):
     assert new_beams.logprobs_and_completions[0][1] == "this is the third time"
 
 
+def load_gpt2_weights(model, reference_gpt2):
+
+    transformer_weight_map = {}
+    for weight_name in reference_gpt2.state_dict().keys():
+
+        new_weight = weight_name
+
+        # ln1 -> attention_ln
+        new_weight = new_weight.replace('ln1.w', 'attention_ln.gamma')
+        new_weight = new_weight.replace('ln1.b', 'attention_ln.beta')
+        # ln2 -> mlp_ln
+        new_weight = new_weight.replace('ln2.w', 'mlp_ln.gamma')
+        new_weight = new_weight.replace('ln2.b', 'mlp_ln.beta')
+        # ln_final.w, ln_final.b -> ln_final.gamma, ln_final.beta
+        new_weight = new_weight.replace('ln_final.w', 'ln_final.gamma')
+        new_weight = new_weight.replace('ln_final.b', 'ln_final.beta')
+        # attn -> attention_layer
+        new_weight = new_weight.replace('attn', 'attention_layer')
+        # W_in, b_in -> W_hidden, b_hidden
+        new_weight = new_weight.replace('W_in', 'W_hidden').replace('b_in', 'b_hidden')
+
+        transformer_weight_map[weight_name] = new_weight
+
+    # map weights
+    mapped_weights = {}
+    for key in reference_gpt2.state_dict().keys():
+        if not transformer_weight_map.get(key, None):
+            mapped_weights[key] = trained_weights[key]
+        else:
+            mapped_weights[transformer_weight_map[key]] = reference_gpt2.state_dict()[key]
+
+    model.load_state_dict(mapped_weights, strict=False)
+    return model
+
 
 def main():
 
-    tokenizer = transformers.GPT2Tokenizer.from_pretrained('gpt2')
-    model = MiniGPT(6, tokenizer.vocab_size, 1024, 256, 12, 64)
+    reference_gpt2 = EasyTransformer.from_pretrained(
+        "gpt2-small", fold_ln=False, center_unembed=False, center_writing_weights=False).to('cpu')
+    tokenizer = reference_gpt2.tokenizer
+    model = MiniGPT(12, reference_gpt2.tokenizer.vocab_size, 1024, 768, 12, 64)
+    # load gpt2 weights
+    model = load_gpt2_weights(model, reference_gpt2)
     
     beams = Beams(
         model,
@@ -35,3 +74,7 @@ def main():
         ])
     )
 
+    test_beam_generation(beams)
+
+if __name__ == '__main__':
+    main()
