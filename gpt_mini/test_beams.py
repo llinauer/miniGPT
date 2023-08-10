f5c628291990bbc2edb3b208bf765f16ada20004
Lukas Linauer <l.linauer@gmx.at>
2023-08-10 11:19:28 +0200

    Lint test_beams.py

--- gpt_mini/test_beams.py
+++ gpt_mini/test_beams.py
@@ -5,13 +5,16 @@ Test the Beams class in generate.py
 """
 
 import torch
-import transformers
+from transformer_lens import EasyTransformer
 from model import MiniGPT
 from generate import Beams
-from transformer_lens import EasyTransformer
 
 
 def test_beam_generation(model, tokenizer):
+    """ Test the beam generation method of Beams class
+    :param model: nn.Module, transformer model
+    :param tokenizer: transformers.tokenizer, tokenizer to use
+    """
 
     # first test case
     beams = Beams( model, tokenizer,
@@ -35,33 +38,42 @@ def test_beam_generation(model, tokenizer):
             [464, 1181,  286,  435, 8480,  318] # the state of alaska is
         ])
     )
-    
+
     new_beams = beams.generate(toks_per_beam=15)
     new_beams.print()
     assert new_beams.logprobs_and_completions[9][1] == "The state of alaska is located"
 
 
 def test_beam_filtering(model, tokenizer):
+    """ Test the beam filtering method of Beams class
+    :param model: nn.Module, transformer model
+    :param tokenizer: transformers.tokenizer, tokenizer to use
+    """
 
     logprob_sums = torch.tensor([-1.0, -2.0])
     tokens = torch.tensor([
         [19485, 13], # Stop .
         [19485, tokenizer.eos_token_id] # Stop EOS
     ])
-    
+
     beams_with_eos = Beams(model, tokenizer, logprob_sums, tokens)
     best_beams, early_terminations = beams_with_eos.filter(2)
-    
+
     torch.testing.assert_close(best_beams.logprob_sums, logprob_sums[[0]])
     torch.testing.assert_close(best_beams.tokens, tokens[[0]])
-    
+
     assert early_terminations.logprobs_and_completions == [(-2.0, "Stop" + tokenizer.eos_token)]
-    
+
     print("All tests for `filter` passed!")
 
 
 
 def load_gpt2_weights(model, reference_gpt2):
+    """ Load the fully trained gpt2 weights
+    :param model: nn.Module, transformer model
+    :param reference.gpt2: transformer_lense.HookedTransformer, fully trained gpt2 transformer
+    :return: nn.Module, model with loaded weights
+    """
 
     transformer_weight_map = {}
     for weight_name in reference_gpt2.state_dict().keys():
@@ -88,7 +100,7 @@ def load_gpt2_weights(model, reference_gpt2):
     mapped_weights = {}
     for key in reference_gpt2.state_dict().keys():
         if not transformer_weight_map.get(key, None):
-            mapped_weights[key] = trained_weights[key]
+            mapped_weights[key] = reference_gpt2.state_dict()[key]
         else:
             mapped_weights[transformer_weight_map[key]] = reference_gpt2.state_dict()[key]
 
@@ -97,15 +109,15 @@ def load_gpt2_weights(model, reference_gpt2):
 
 
 def main():
-
+    """ main function, load model, load gpt2 weights and run tests """
     reference_gpt2 = EasyTransformer.from_pretrained(
         "gpt2-small", fold_ln=False, center_unembed=False, center_writing_weights=False).to('cpu')
     tokenizer = reference_gpt2.tokenizer
     model = MiniGPT(12, reference_gpt2.tokenizer.vocab_size, 1024, 768, 12, 64)
     # load gpt2 weights
     model = load_gpt2_weights(model, reference_gpt2)
-    
 
+    # run tests
     test_beam_generation(model, tokenizer)
     test_beam_filtering(model, tokenizer)
 
